69c7ccbfdf92b062b52476f541442c45577f4698
rafael gonzalez <rafaelg@indeed.com>
2022-02-23 22:23:46 +0000

    wait for database

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -38,6 +38,5 @@ class AdminSiteTests(TestCase):
         """Test that the create user page works"""
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
-        
-        self.assertEqual(res.status_code, 200)
 
+        self.assertEqual(res.status_code, 200)
